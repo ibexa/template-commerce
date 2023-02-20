3f358ba0118fa394a63ad6c6b3dc304e51d48a34
Ibexa Jenkins <jenkins@ibexa.co>
2023-02-20 15:56:52 +0000

    Template for v4.4.1

---
+++ public/bundles/ibexastorefront/js/store.product.list.filters.js
@@ -0,0 +1,31 @@
+(function (global, doc) {
+    const formSelects = doc.querySelectorAll('.ibexa-store-form .ibexa-store-select');
+    const formChoices = doc.querySelectorAll('.ibexa-store-form .ibexa-store-choice input');
+    const formTextInputs = doc.querySelectorAll('.ibexa-store-form .ibexa-store-input');
+    const filterLabels = doc.querySelectorAll('.ibexa-store-filter__label');
+    const submitForm = function () {
+        this.form.submit();
+    };
+    const submitOnClear = function (event) {
+        const isClearBtnClickEvent = event instanceof InputEvent;
+
+        if (isClearBtnClickEvent) {
+            return;
+        }
+
+        submitForm.call(this);
+    };
+    const toggleFilter = ({ currentTarget: filterLabel }) => {
+        const filter = filterLabel.closest('.ibexa-store-filter');
+
+        filter.classList.toggle('ibexa-store-filter--collapsed');
+    };
+
+    formSelects.forEach((formSelect) => formSelect.addEventListener('change', submitForm, false));
+    formChoices.forEach((formChoice) => formChoice.addEventListener('change', submitForm, false));
+    formTextInputs.forEach((formTextInput) => {
+        formTextInput.addEventListener('change', submitForm, false);
+        formTextInput.addEventListener('input', submitOnClear, false);
+    });
+    filterLabels.forEach((filterLabel) => filterLabel.addEventListener('click', toggleFilter, false));
+})(window, window.document);
