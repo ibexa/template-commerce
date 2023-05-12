e235b4257c6e1a58b7b01960dd0f5e0b699eda53
Ibexa Jenkins <jenkins@ibexa.co>
2023-05-12 10:54:49 +0000

    Template for v4.5.0

---
+++ public/bundles/ibexashipping/js/edit.form.validator.js
@@ -0,0 +1,31 @@
+(function (global, doc, ibexa) {
+    const editForm = doc.querySelector('.ibexa-shipping-edit__form');
+    const submitBtn = editForm.querySelector('.ibexa-shipping-edit__submit-btn');
+    const inputsToValidate = [...editForm.querySelectorAll('.ibexa-input[required]')];
+    const validateField = (input) => {
+        const field = input.closest('.form-group');
+
+        return ibexa.helpers.formValidation.validateIsEmptyField(field);
+    };
+    const validateForm = (event) => {
+        event.preventDefault();
+
+        const isFormValid = inputsToValidate.map(validateField).every(({ isValid }) => isValid);
+
+        if (isFormValid) {
+            editForm.submit();
+        }
+    };
+    const attachTriggerToValidateFields = () => {
+        inputsToValidate.forEach((input) => {
+            if (input.tagName.toLowerCase() === 'select') {
+                input.addEventListener('change', () => validateField(input), false);
+            } else {
+                input.addEventListener('blur', () => validateField(input), false);
+            }
+        });
+    };
+
+    attachTriggerToValidateFields();
+    submitBtn.addEventListener('click', validateForm, false);
+})(window, window.document, window.ibexa);
